afdd880ef360a4c3d46bf48822fc38f9a60f717a
Antti Rae <anttirae@gmail.com>
2023-04-30 08:24:45 +0300

    update .gitigore, include local_settings.py for github actions. update run_tests.yml

---
+++ urlshortener/local_settings.py
@@ -0,0 +1,21 @@
+# local settings.
+# use this file to assign settings for the current environment.
+# This one is used for github actions
+# In production set debug to false and use proper secret key
+
+SECRET_KEY = 'django-insecure-y9xb2c#sp#*5i-tyzz%jrjy5iriy#bdh#n&$+=hhb8*kgde8m^'
+DEBUG = True
+
+
+# Database
+# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'test_db',
+        'USER': 'postgres',
+        'PASSWORD': 'password',
+        'HOST': '0.0.0.0',
+        'PORT': '5432',
+    }
+}
